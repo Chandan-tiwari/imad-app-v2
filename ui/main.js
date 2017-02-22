72ee3bc90c473ab54c1b2a6aad4f0e462e48881f
Chandan-tiwari <chandanjnv42@gmail.com>
2017-02-23 02:19:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,17 +3,16 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML='NEW VALUE';
 
+
+
 //move the image
 var img= document.getElementById('madi');
     var marginLeft=0;
-    function moveRight(){
+    function moveRight() {
         marginLeft=marginLeft+10;
-        img.style.marginLeft=marginleft+'px';
+        img.style.marginLeft=marginleft+ 'px';
         }
-        
-        
-        img.onclick=function()
-        {
+        img.onclick=function(){
           var interval=setInterval(moveRight,100);
     
         };
