6751029af2e4ba88afd405fca5a27fcdb8c50e80
Chandan-tiwari <chandanjnv42@gmail.com>
2017-02-23 02:01:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,10 @@ console.log('Loaded!');
 //change the text of maoin-text div
 var element=document.getElementById('main-text');
 element.innerHTML='NEW VALUE';
+
+//move the image
+var img= document.getElementById('madi');
+img.onclick=function(){
+  img.style.marginLeft='100px';
+    
+};
